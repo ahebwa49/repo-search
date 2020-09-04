ad3b6c2019f2398a3da7b45a3ec3ebb77ba8b1f8
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-09-04 11:11:30 +0300

    add apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 import { Header } from './Header';
-import logo from './logo.svg';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    authorization: `Bearer our-bearer-token`
+  }
+});
 
 class App extends React.Component {
   public constructor(props: {}) {
